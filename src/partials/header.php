3f4751e642859ea35d32e268bc269e6ae6e8aad9
TanguyScholtes <soultaker720@hotmail.com>
2018-11-28 10:39:53 +0100

    added database scheme (my personnal take on it) & swapped stylesheets order to avoid reset of Julie's css

--- src/partials/header.php
+++ src/partials/header.php
@@ -10,8 +10,8 @@
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
 
-    <link rel="stylesheet" href="css/styles.css">
     <link rel="stylesheet" href="css/tanguy_styles.css">
+    <link rel="stylesheet" href="css/styles.css">
 
     <link rel="shortcut icon" href="images/favicon.ico" />
 </head>
